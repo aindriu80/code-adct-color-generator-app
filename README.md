bfec7355bbeefdca39ab11b95f30ef8bc0933ffc
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-21 13:00:09 +0100

    Project Setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # code-adct-color-generator-app
+
+# Using Values JS
+
+[value.js] (https://github.com/noeldelgado/values.js)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import SingleColor from './SingleColor.jsx';
+import Values from 'values.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Color Generator App</h2>
+    </div>
+  );
+}
+
+export default App;
